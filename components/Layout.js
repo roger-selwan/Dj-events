3f240978028461e575b2bd1aff268f8155b728e6
Roger Selwan <roger@fusionsecond.com>
2021-08-31 19:00:32 +0300

    ADD: created new components

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.css';
+import Showcase from './Showcase';
+import { useRouter } from 'next/router';
+import styles from '@/styles/Layout.module.css';
 
 export default function Layout({title, keywords, description, children}) {
+    const router = useRouter();
+
 	return (
 		<div>
 			<Head>
@@ -13,6 +17,8 @@ export default function Layout({title, keywords, description, children}) {
 			</Head>
 
             <Header />
+
+            {router.pathname === '/' && <Showcase />}
 
             <div className={styles.container}>
                 {children}
