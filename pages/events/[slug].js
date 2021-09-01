163232654741ac9c4aa00517fd40f6bc514c4d13
Roger Selwan <roger@fusionsecond.com>
2021-09-01 17:47:05 +0300

    FIX: fixed the api url

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -23,11 +23,11 @@ export default function EventPage({ evt }) {
 						<FaTimes /> Delete Event
 					</a>
 				</div>
-				{evt.date} at {evt.time}
+				{new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
 				<h1>{evt.name}</h1>
 				{evt.image && (
 					<div className={styles.image}>
-						<Image src={evt.image} width={960} height={600} />
+						<Image src={evt.image.formats.medium.url} width={960} height={600} />
 					</div>
 				)}
 				<h3>Performers:</h3>
@@ -48,7 +48,7 @@ export default function EventPage({ evt }) {
 }
 
 export async function getStaticPaths() {
-	const res = await fetch(`${API_URL}/api/events`);
+	const res = await fetch(`${API_URL}/events`);
 	const events = await res.json();
 
 	const paths = events.map((evt) => ({
@@ -62,7 +62,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-	const res = await fetch(`${API_URL}/api/events/${slug}`);
+	const res = await fetch(`${API_URL}/events?slug=${slug}`);
 	const events = await res.json();
 
 	return {
