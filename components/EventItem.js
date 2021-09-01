06d02137837553d4a51ff511abc92289cb1d29a1
Roger Selwan <roger@fusionsecond.com>
2021-09-01 17:46:27 +0300

    FIX: fix the calling of the image

--- components/EventItem.js
+++ components/EventItem.js
@@ -6,12 +6,12 @@ export default function EventItem({ evt }) {
 	return (
 		<div className={styles.event}>
 			<div className={styles.img}>
-				<Image src={evt.image ? evt.image : '/image/event-default.png'} width={170} height={100} />
+				<Image src={evt.image ? evt.image.formats.thumbnail.url : '/image/event-default.png'} width={170} height={100} />
 			</div>
 
             <div className={styles.info}>
             <span>
-                {evt.date} at {evt.time}
+                {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
             </span>
                 <h3>{evt.name}</h3>
             </div>
