1878e649d48f0c03a0e2dbfc635eae5205e862fb
Roger Selwan <roger@fusionsecond.com>
2021-09-03 10:52:20 +0300

    EDIT: added delete functionality

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,20 +1,37 @@
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
 import styles from '@/styles/Event.module.css';
 import { API_URL } from '@/config/index';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function EventPage({ evt }) {
-	const deleteEvent = (e) => {
-		console.log('delete');
+	const router = useRouter();
+
+	const deleteEvent = async (e) => {
+		if (confirm('Are you sure')) {
+			const res = await fetch(`${API_URL}/events/${evt.id}`, {
+				method: 'DELETE'
+			})
+
+			const data = await res.json();
+
+			if (!res.ok) {
+				toast.error(data.message);
+			} else {
+				router.push('/events');
+			}
+		}
 	};
 
 	return (
 		<Layout>
 			<div className={styles.event}>
 				<div className={styles.controls}>
-					<Link href={`events/edit/${evt.id}`}>
+					<Link href={`/events/edit/${evt.id}`}>
 						<a>
 							<FaPencilAlt /> Edit Event
 						</a>
@@ -25,6 +42,7 @@ export default function EventPage({ evt }) {
 				</div>
 				{new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
 				<h1>{evt.name}</h1>
+				<ToastContainer />
 				{evt.image && (
 					<div className={styles.image}>
 						<Image src={evt.image.formats.medium.url} width={960} height={600} />
