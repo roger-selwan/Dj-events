f5855d99d7f5a97e811471b2c7ff3cc5f00a8ae9
Roger Selwan <roger@fusionsecond.com>
2021-09-03 10:51:56 +0300

    ADD: created id for the url

---
+++ pages/events/edit/[id].js
@@ -0,0 +1,143 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Image from 'next/image';
+import {FaImage} from 'react-icons/fa';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Form.module.css';
+import moment from 'moment';
+
+export default function EditEventPage({evt}) {
+	const [ values, setValues ] = useState({
+		name: evt.name,
+		performers: evt.performers,
+		venue: evt.venue,
+		address: evt.address,
+		date: evt.date,
+		time: evt.time,
+		description: evt.description
+	});
+
+    const [imagePreview, setImagePreview] = useState(evt.image ? evt.image.formats.thumbnail.url : null)
+
+	const router = useRouter();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		// Validation
+		const hasEmptyFields = Object.values(values).some((element) => element === '');
+
+		if (hasEmptyFields) {
+			toast.error('Please fill all fields');
+		}
+
+        const res = await fetch(`${API_URL}/events/${evt.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+
+        if(!res.ok) {
+            toast.error('Something went wrong')
+        } else {
+            const evt = await res.json();
+            router.push(`/events/${evt.slug}`);
+        }
+	};
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setValues({ ...values, [name]: value });
+	};
+
+	return (
+		<Layout title="Update new event">
+			<Link href="/events">Go Back</Link>
+			<h1>Edit Events</h1>
+			<ToastContainer />
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<div className={styles.grid}>
+					<div>
+						<label htmlFor="name">Event Name</label>
+						<input type="text" id="name" name="name" value={values.name} onChange={handleInputChange} />
+					</div>
+					<div>
+						<label htmlFor="performers">Performers</label>
+						<input
+							type="text"
+							name="performers"
+							id="performers"
+							value={values.performers}
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="venue">Venue</label>
+						<input type="text" name="venue" id="venue" value={values.venue} onChange={handleInputChange} />
+					</div>
+					<div>
+						<label htmlFor="address">Address</label>
+						<input
+							type="text"
+							name="address"
+							id="address"
+							value={values.address}
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="date">Date</label>
+						<input type="date" name="date" id="date" value={moment(values.date).format('yyyy-MM-DD')} onChange={handleInputChange} />
+					</div>
+					<div>
+						<label htmlFor="time">Time</label>
+						<input type="text" name="time" id="time" value={values.time} onChange={handleInputChange} />
+					</div>
+				</div>
+				<div>
+					<label htmlFor="description">Event Description</label>
+					<textarea
+						type="text"
+						name="description"
+						id="description"
+						value={values.description}
+						onChange={handleInputChange}
+					/>
+				</div>
+				<input type="submit" value="Update Event" className="btn" />
+			</form>
+
+            <h2>Event Image</h2>
+            {imagePreview ? (
+                <Image src={imagePreview} height={100} width={170} />
+            ) : (
+                <div>
+                    <p>No image uploaded</p>
+                </div>
+            )}
+
+            <div>
+                <button className='btn-secondary'>
+                    <FaImage /> Set image
+                </button>
+            </div>
+		</Layout>
+	);
+}
+
+export async function getServerSideProps({params: {id}}) {
+    const res = await fetch(`${API_URL}/events/${id}`);
+    const evt = await res.json();
+
+    return  {
+        props: {
+            evt
+        }
+    }
+}
