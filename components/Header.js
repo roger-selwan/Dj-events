df31024422a9e34915d5eb6f0ad01f2171b91834
Roger Selwan <roger@fusionsecond.com>
2021-09-06 11:11:52 +0300

    EDIT: added login and registration links

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Search from './Search';
-import styles from '../styles/Header.module.css'
+import styles from '../styles/Header.module.css';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 export default function Header() {
     return (
@@ -23,6 +24,11 @@ export default function Header() {
                     <li>
                         <Link href='/events/add'>
                             <a>Add Event</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/account/login'>
+                            <a className='btn-secondary btn-icon'> <FaSignInAlt /> Login</a>
                         </Link>
                     </li>
                 </ul>
